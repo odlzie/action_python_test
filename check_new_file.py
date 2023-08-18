b4f00991f1c9b0cc888bc3bc0e9cb9731376a164
odlzie <liuzhao9ster@gmail.com>
2023-08-18 23:51:36 +0100

    workflow test

--- check_new_file.py
+++ check_new_file.py
@@ -7,17 +7,20 @@ class get_input_file:
 
     def get_latest_csv_file(self, input_dir):
         if os.path.isdir(input_dir):
-            latest_file = max(glob.glob(os.path.join(input_dir, '*.csv')), key=os.path.getctime)
-
-            if latest_file is None:
-                raise ValueError('no csv file in input dir')
+            try:
+                latest_file = max(glob.glob(os.path.join(input_dir, '*.csv')), key=os.path.getctime)
+            except:
+                raise ValueError('No csv file in input directory.')
         else:
             raise FileNotFoundError
+        return latest_file
+
 
     def write_to_env(self):
+        print(self.latest_csv)
         env_file = os.getenv('GITHUB_ENV')
-        with open(env_file, "a") as f:
-            f.write("my_csv={}".format(self.latest_csv))
+        with open(env_file, "a") as myfile:
+            myfile.write("my_csv={}".format(self.latest_csv))
 
 if __name__ == "__main__":
     my_class = get_input_file('./data/input')
